853149f37fb8d247d4fcbc95aed5ecb8f8e1c870
Raman Dubey <raman.dubey1988@gmail.com>
2019-11-20 21:04:14 +0100

    Updated Code

--- js/script.js
+++ js/script.js
@@ -203,7 +203,7 @@ $(function() {
 
   $(function() {
     $(".typed").typed({
-      strings: ["a Developer", "a Consultant", "IT Operation Lead", "pursuing EMBA", "Human :)"],
+      strings: ["Cloud Architect", "a Consultant", "IT Operation Lead", "EMBA Candidate", "Human :)"],
       // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
       stringsElement: null,
       // typing speed
